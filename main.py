0d6527c2519210ee128046c1953fe8a1d5ef6071
Jess <jjohnson4927@gmail.com>
2018-04-04 13:16:42 -0500

    create specific error messages in validation fun

--- main.py
+++ main.py
@@ -16,28 +16,55 @@ def index():
     return redirect("/signup")
 
 def valid_email(email):
-    if "@" in email and 2 < len(email) < 21:
-        return True
+    if email.count("@") != 1 and (2 < len(email) < 21) == False:
+        valid_email.error = 'Invalid email. must contain "@", must be 3-20 characters in length, no spaces.'
+    elif email == '':
+        valid_email.error = ''
     else:
-        return False
+        valid_email.error = ''
 def valid_usrnm(usrnm):
-    if ' ' in usrnm or 2 < len(usrnm) < 21 or usrnm == '':
-        return False
+    if ' ' in usrnm or 2 < len(usrnm) < 21:
+        valid_usrnm.error = 'Invalid username, must be 3-20 characters in length, no spaces.'
+    elif usrnm == '':
+        valid_usrnm.error = 'Field'
     else:
-        return True
+        valid_usrnm.error = ''
+    
+    return valid_usrnm.error
+
 def valid_psswrd(psswrd):
-    if ' ' in psswrd or 2 < len(psswrd) < 21 or psswrd == '':
-        return False
+    if ' ' in psswrd or 2 < len(psswrd) < 21:
+        valid_psswrd.error = 'Invalid password, must be 3-20 characters in length, no spaces.'
+    elif psswrd == '':
+        valid_psswrd.error = 'Field'
     else:
-        return True
-def psswrd_match(psswrd, repeat):
-    if psswrd == repeat:
-        return True
+        valid_psswrd.error = ''
+    
+    return valid_psswrd.error
+
+def psswrd_match(psswrd, confirm):
+    if psswrd == confirm:
+        psswrd_match.error = ''
     else:
-        return False
+        psswrd_match.error = 'Passwords do not match.'
+
+    return psswrd_match.error
 
 @app.route("/signup")
 def display_signup():
     template = jinja_env.get_template('signup_page.html')
     return template.render()
+
+@app.route("/signup", methods=['POST'])
+def validate_signup():
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['psw']
+    confirm_psw = request.form['psw-repeat']
+
+    usrnm_error = ''
+    email_error = ''
+    psw_error = ''
+    confirm_error = ''
+
 app.run()
