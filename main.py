29ab3a313d2d0ca5a7597508d6109598b4ece503
Jess <jjohnson4927@gmail.com>
2018-04-05 21:32:37 -0500

    fix validation bug in email field, format templates

--- main.py
+++ main.py
@@ -16,29 +16,32 @@ def index():
     return redirect("/signup")
 
 def valid_email(email):
-    if email.count("@") != 1 and (2 < len(email) < 21) == False:
-        valid_email.error = 'Invalid email. must contain "@", must be 3-20 characters in length, no spaces.'
-    elif email == '':
-        valid_email.error = ''
+    if email != '':
+        if email.count("@") != 1 or email.count(".") != 1 or not (2 < len(email) < 21):
+            valid_email.error = 'Invalid email. must contain "@" and ".", must be 3-20 characters in length, no spaces.'
+        else:
+          valid_email.error = ''
     else:
         valid_email.error = ''
 
     return valid_email.error
+
+    
 def valid_usrnm(usrnm):
-    if ' ' in usrnm or 2 < len(usrnm) < 21:
+    if ' ' in usrnm or not (2 < len(usrnm) < 21):
         valid_usrnm.error = 'Invalid username, must be 3-20 characters in length, no spaces.'
     elif usrnm == '':
-        valid_usrnm.error = 'Field'
+        valid_usrnm.error = 'Field required.'
     else:
         valid_usrnm.error = ''
     
     return valid_usrnm.error
 
 def valid_psswrd(psswrd):
-    if ' ' in psswrd or 2 < len(psswrd) < 21:
+    if ' ' in psswrd or not (2 < len(psswrd) < 21):
         valid_psswrd.error = 'Invalid password, must be 3-20 characters in length, no spaces.'
     elif psswrd == '':
-        valid_psswrd.error = 'Field'
+        valid_psswrd.error = 'Field required.'
     else:
         valid_psswrd.error = ''
     
@@ -57,6 +60,12 @@ def display_signup():
     template = jinja_env.get_template('signup_page.html')
     return template.render()
 
+@app.route("/welcome")
+def welcome_page():
+    username = request.args.get('username')
+    template = jinja_env.get_template('welcome_page.html')
+    return template.render(username=username)
+
 @app.route("/signup", methods=['POST'])
 def validate_signup():
     username = request.form['username']
@@ -68,5 +77,16 @@ def validate_signup():
     email_error = valid_email(email)
     psw_error = valid_psswrd(password)
     confirm_error = psswrd_match(password, confirm_psw)
+
+    if not usrnm_error and not email_error and not psw_error and not confirm_error:
+
+        return redirect('/welcome?username={0}'.format(username))
+    
+    else:
+        template = jinja_env.get_template('signup_page.html')
+
+        return template.render(username=username, email=email, usrnm_error=usrnm_error, email_error=email_error, 
+        psw_error=psw_error, confirm_error=confirm_error)
+        
 
 app.run()
